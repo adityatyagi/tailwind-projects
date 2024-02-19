200b20dabb21a30b864fb5ba1c9d406358e07b03
adityatyagi <tyagi.aditya844747@gmail.com>
2024-02-19 14:17:24 +0530

    animation mobile menu

---
+++ rockets/build/js/main.js
@@ -0,0 +1,19 @@
+const initApp = () => {
+    const hamburgerMenuBtn = document.getElementById('hamburger-button');
+    console.log("🚀 ~ initApp ~ hamburgerMenuBtn:", hamburgerMenuBtn)
+
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        console.log("🚀 ~ toggleMenu ~ toggleMenu:", toggleMenu)
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        // hamburgerMenuBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerMenuBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+};
+
+// init the app after the DOM has been completely loaded;
+document.addEventListener('DOMContentLoaded', initApp);
